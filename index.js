cad40c28f4cf5ee04e69bea400f963b57256247e
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2023-11-11 12:09:45 -0400

    adicionando menu mobile

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-function menuShow() {
-    let menuMobile = document.querySelector(".mobileMenu")
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open')
-    }else{
-        menuMobile.classList.add('open')
+function toggleMenu() {
+    let menuMobile = document.querySelector(".menuHeader");
+    let iconCloseMobile = document.querySelector(".fa-xmark")
+    let iconOpenMobile = document.querySelector(".fa-bars")
+
+    if (menuMobile.style.display === "none" || menuMobile.style.display === "") {
+        menuMobile.style.display = "flex";
+        iconCloseMobile.style.display = "flex"
+        iconOpenMobile.style.display = "none"
+    } else {
+        menuMobile.style.display = "none";
+        iconCloseMobile.style.display = "none"
+        iconOpenMobile.style.display = "flex"
     }
+
 }
+
+document.querySelectorAll('.menuHeader a').forEach(item => {
+    item.addEventListener('click', () => {
+        let menuMobile = document.querySelector(".menuHeader");
+        let iconCloseMobile = document.querySelector(".fa-xmark")
+        let iconOpenMobile = document.querySelector(".fa-bars")
+        menuMobile.style.display = "none";
+        iconCloseMobile.style.display = "none"
+        iconOpenMobile.style.display = "flex"
+    });
+});
+
